2dace0b433c093769e4b6365f2931f2b48245fc5
Nicolas Ignacio <nicoigna100@gmail.com>
2023-01-30 18:32:35 -0300

    Resolucion ej 2

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,18 @@
 /*
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
-function mostrar()
-{
-	alert("ok");
+function mostrar(){
+//alert("ok");
+
+// var edad;
+let nombre;
+let nombreCompleto; // notacion lower camel case
+
+// nombre = "Nicolas"; // asignacion de string.
+nombre = prompt("ingrese un nombre");
+
+//mostrar por alert
+alert (nombre)
+
 }
 
